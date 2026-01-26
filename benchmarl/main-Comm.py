869d6fbd24d30fac9c48b2cab6eaa3044f1c12ba
valentincuzin <valentincuzin.pro@ikmail.com>
2026-01-26 16:56:31 +0100

    ready for first benchmark

--- benchmarl/main-Comm.py
+++ benchmarl/main-Comm.py
@@ -1,21 +1,25 @@
 from benchmarl.algorithms import IppoConfig
 from benchmarl.environments import VmasTask
-from benchmarl.experiment import Experiment, ExperimentConfig, Callback
+from benchmarl.experiment import ExperimentConfig
+from benchmarl.benchmark import Benchmark
 from benchmarl.models import MlpConfig, GnnConfig, SequenceModelConfig
 
-from torch_geometric.nn import GCNConv, GATv2Conv, Sequential
+from torch_geometric.nn import GATv2Conv
 
 if __name__ == "__main__":
-    # Loads from "benchmarl/conf/algorithm/masac.yaml"
     algorithm_config = IppoConfig.get_from_yaml()
-
-    # Some basic other configs
+    algorithm_config.share_param_critic = False
     experiment_config = ExperimentConfig.get_from_yaml()
-    task = VmasTask.NAVIGATION.get_from_yaml()
-    gnn_module_config = GnnConfig(
+    experiment_config.share_policy_params = False
+
+    navigation = VmasTask.NAVIGATION.get_from_yaml()
+    joint_passage_size = VmasTask.JOINT_PASSAGE_SIZE.get_from_yaml()
+    transport = VmasTask.TRANSPORT.get_from_yaml()
+
+    gat_module_config = GnnConfig(
         topology="from_pos",
         self_loops=False,
-        gnn_class=GCNConv,
+        gnn_class=GATv2Conv,
         position_key="pos",
         pos_features=2,
         velocity_key="vel",
@@ -23,15 +27,15 @@ if __name__ == "__main__":
         exclude_pos_from_node_features=True,
         edge_radius=1,
     )
-    model_config = SequenceModelConfig([gnn_module_config, MlpConfig.get_from_yaml()], [256])
+    gat_model_config = SequenceModelConfig([gat_module_config, MlpConfig.get_from_yaml()], [256])
     critic_model_config = MlpConfig.get_from_yaml()
 
-    experiment = Experiment(
-        task=task,
-        algorithm_config=algorithm_config,
-        model_config=model_config,
+    benchmark = Benchmark(
+        algorithm_configs=[algorithm_config],
+        model_config=gat_model_config,
         critic_model_config=critic_model_config,
-        seed=0,
-        config=experiment_config,
+        experiment_config=experiment_config,
+        tasks=[navigation, joint_passage_size, transport],
+        seeds={0, 1, 2}
     )
-    experiment.run()
+    benchmark.run()
